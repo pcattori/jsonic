b77c09e5d5b6eeed9924fe9bf3168ecf1f01f51f
Pedro Cattori <pedro.cattori@tamr.com>
2016-10-11 14:17:23 -0400

    playing nice with unicode

--- jsonic/io.py
+++ jsonic/io.py
@@ -1,5 +1,7 @@
+from __future__ import absolute_import, print_function
 import collections
 import json
+from io import open
 
 def stream_file(f):
     '''TODO docstring'''
@@ -9,7 +11,7 @@ def stream_file(f):
 def write_file(records, f):
     '''TODO docstring'''
     for record in records:
-        print(json.dumps(record), file=f)
+        print(json.dumps(record, ensure_ascii=False), file=f)
 
 def stream_filepath(filepath, encoding='utf8'):
     '''TODO docstring'''
