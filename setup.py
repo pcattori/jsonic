72df4a841dc5ecef190fe9f43f19f4712903dae9
Pedro Cattori <pedro.cattori@tamr.com>
2016-10-11 14:55:23 -0400

    major version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='jsonic',
-  version='0.1.0',
+  version='1.0.0',
   description='Utilities for handling streams of JSON objects',
   url='https://github.com/pcattori/jsonic',
   author='Pedro Cattori',
